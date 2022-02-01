c4fb72ae806f9da68d8d155a1360014b76256ae5
adamhinton <adam.d.hinton@gmail.com>
2022-02-01 16:56:24 -0500

    cleaned up code

--- api/plants/plants-router.js
+++ api/plants/plants-router.js
@@ -27,7 +27,6 @@ router.get("/", (req, res, next) => {
 router.get("/:id", (req, res, next) => {
   // A plant can be selected to present user
   //needs id validation
-  //   console.log("req.params.id:", req.params.id);
   const { id } = req.params;
 
   Plant.getPlantById(id)
@@ -37,9 +36,8 @@ router.get("/:id", (req, res, next) => {
     .catch((err) => {
       res.status(500).json({ message: err.message });
     });
-
-  //   res.json({ message: "plants id get endpoint successful call" });
 });
+
 // router.verb('/', (req, res, next) => {
 
 // })
