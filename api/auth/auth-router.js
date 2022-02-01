afd1e3e81f2a2be34166d0100591271437b1e73b
Justin Peisker <justinpeisker@gmail.com>
2022-02-01 08:32:36 -0800

    endpoint test

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -7,9 +7,18 @@ router.post('/register', (req, res, next) => {
     // user can sign-up / create an account by providing a unique username, a valid mobile phoneNumber and a password.
     res.json({message: 'register endpoint successful call'})
 })
+
 router.post('/login', (req, res, next) => {
     // user can login to an authenticated session using the credentials provided at account creation / signup.
     res.json({message: 'login endpoint successful call'})
+})
+
+// endpoints for testing
+router.get('/login', (req, res, next) => {
+    res.json({message: 'login endpoint successful call'})
+})
+router.get('/register', (req, res, next) => {
+    res.json({message: 'register endpoint successful call'})
 })
 
 module.exports = router
