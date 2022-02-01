80b854170f173455390b3853f3688d6ca0ebe525
adamhinton <adam.d.hinton@gmail.com>
2022-02-01 17:45:58 -0500

    added dummy plant names to help me test deletes

--- data/seeds/01-plant.js
+++ data/seeds/01-plant.js
@@ -1,8 +1,22 @@
-exports.seed = function(knex) {
-    return knex('plants').insert({
-        plant: 'Dahlia Pinnata',
-        nickname: 'Dolly',
-        species: 'Pinnata',
-        h2ofrequency: '2x a week'
-    })
-}
+exports.seed = function (knex) {
+  return knex("plants").insert([
+    {
+      plant: "Dahlia Pinnata",
+      nickname: "Dolly",
+      species: "Pinnata",
+      h2ofrequency: "2x a week",
+    },
+    {
+      plant: "Test",
+      nickname: "Dolfdsafsafsaly",
+      species: "Pinnatfdasfasa",
+      h2ofrequency: "2x a wefdsafsdafasek",
+    },
+    {
+      plant: "Dahlia fdsfsdafsaPinnata",
+      nickname: "Ddfasfdsaasaolly",
+      species: "Pinaafdsafsadggetgretnata",
+      h2ofrequency: "2x a ewrwqerwqweek",
+    },
+  ]);
+};
