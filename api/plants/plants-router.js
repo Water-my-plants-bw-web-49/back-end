b5f7dc36a95ddf14002c9f8c95c7478bf069ac56
Justin Peisker <justinpeisker@gmail.com>
2022-02-01 08:19:46 -0800

    wip

--- api/plants/plants-router.js
+++ api/plants/plants-router.js
@@ -1,23 +1,23 @@
 const router = require('express').Router()
 
 
-router.post('/plants', (req, res, next) => {
+router.post('/', (req, res, next) => {
     // Authenticated user can Create, a plant
     res.json({message: 'plants post endpoint successful call'})
 })
-router.put('/plants/:id', (req, res, next) => {
+router.put('/:id', (req, res, next) => {
     // Authenticated user can update a plant
     res.json({message: 'plants id put endpoint successful call'})
 })
-router.delete('/plants/:id', (req, res, next) => {
+router.delete('/:id', (req, res, next) => {
     // Authenticated user can delete a plant
     res.json({message: 'plants delete endpoint successful call'})
 })
-router.get('/plants', (req, res, next) => {
+router.get('/', (req, res, next) => {
     // Authenticated user can view a list of created plants.
     res.json({message: 'plants get endpoint successful call'})
 })
-router.get('/plants/:id', (req, res, next) => {
+router.get('/:id', (req, res, next) => {
     // A plant can be selected to present user 
     res.json({message: 'plants id get endpoint successful call'})
 })
