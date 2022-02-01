e297c685b6333c2e06e2c33df1a731a2666cbae7
Justin Peisker <justinpeisker@gmail.com>
2022-02-01 08:01:16 -0800

    testing endpoint

--- api/server.js
+++ api/server.js
@@ -13,6 +13,10 @@ server.use(express.json());
 server.use('/api/auth', authRouter)
 server.use('/api/plants', restricted, plantsRouter)
 
+server.get('/testing', (req, res) => {
+    res.json({message: 'testing message'})
+})
+
 server.use((err, req, res, next) => { 
     res.status(err.status || 500).json({
       message: err.message,
