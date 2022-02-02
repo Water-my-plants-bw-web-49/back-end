d437ecfc3feb913a339777f6babeb8821560a863
Justin Peisker <justinpeisker@gmail.com>
2022-02-02 10:01:17 -0800

    auth router branch re-commit w/plants router

--- api/plants/plants-router.js
+++ api/plants/plants-router.js
@@ -1,28 +1,75 @@
-const router = require('express').Router()
-
-
-router.post('/', (req, res, next) => {
-    // Authenticated user can Create, a plant
-    
-})
-router.put('/:id', (req, res, next) => {
+const router = require("express").Router();
+const Plant = require("./plants-model");
+const {
+  validatePlantId,
+  validatePlantPut,
+  validateNewPlant,
+} = require("./plants-middleware");
+router.post("/", validateNewPlant, async (req, res, next) => {
+  //   res.status(200).json({ message: "posting..." });
+  // Authenticated user can Create, a plant
+  //this needs restricted middleware
+  await Plant.insert(req.body)
+    .then((plant) => {
+      res.status(201).json(plant);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+//status code correct (delete this note later)
+router.put(
+  "/:id",
+  validatePlantId,
+  validatePlantPut,
+  async (req, res, next) => {
     // Authenticated user can update a plant
-    
-})
-router.delete('/:id', (req, res, next) => {
-    // Authenticated user can delete a plant
-    
-})
-router.get('/', (req, res, next) => {
-    // Authenticated user can view a list of created plants.
-    res.json({message: 'plants get endpoint successful call'})
-})
-router.get('/:id', (req, res, next) => {
-    // A plant can be selected to present user 
-    res.json({message: 'plants id get endpoint successful call'})
-})
-// router.verb('/', (req, res, next) => {
-
-// })
-
+    //this needs restricted middleware
+    await Plant.updatePlant(req.params.id, req.body)
+      .then((updatedPlant) => {
+        res.status(200).json(updatedPlant);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+);
+//status code correct (delete this note later)
+router.delete("/:id", validatePlantId, async (req, res, next) => {
+  // Authenticated user can delete a plant
+  //needs restricted middleware
+  const { id } = req.params;
+  await Plant.deletePlant(id)
+    .then(() => {
+      res.status(200).json({ message: "plant deleted" });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+//status code correct (delete this note later)
+router.get("/", (req, res, next) => {
+  // Authenticated user can view a list of created plants.
+  //this needs restricted middleware at least, not sure if it needs anything else. -AH
+  Plant.getPlants()
+    .then((plants) => {
+      res.status(200).json(plants);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+//status code correct (delete this note later)
+router.get("/:id", validatePlantId, (req, res, next) => {
+  // A plant can be selected to present user
+  //this needs restricted middleware
+  const { id } = req.params;
+  Plant.getPlantById(id)
+    .then((plant) => {
+      res.status(200).json(plant);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 module.exports = router;
