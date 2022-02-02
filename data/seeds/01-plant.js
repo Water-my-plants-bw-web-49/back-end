78954db12b1308f7cd6607e9c9285e288b6fff15
adamhinton <adam.d.hinton@gmail.com>
2022-02-01 21:50:23 -0500

    reverted plant seed to original state with only one plant object

--- data/seeds/01-plant.js
+++ data/seeds/01-plant.js
@@ -1,52 +1,8 @@
 exports.seed = function (knex) {
-  return knex("plants").insert([
-    {
-      plant: "Dahlia Pinnata",
-      nickname: "Dolly",
-      species: "Pinnata",
-      h2ofrequency: "2x a week",
-    },
-    {
-      plant: "Test",
-      nickname: "Dolfdsafsafsaly",
-      species: "Pinnatfdasfasa",
-      h2ofrequency: "2x a wefdsafsdafasek",
-    },
-    {
-      plant: "Dahlia fdsfsdafsaPinnata",
-      nickname: "Ddfasffdsfsaaaadsaasaolly",
-      species: "Pinaafdaaaasafsadggetgretnata",
-      h2ofrequency: "2xghggghj a ewrwqerwqweek",
-    },
-    {
-      plant: "Dahlia fdsffdsafasaaasadfsadfsadfdsfsdafsaPinnata",
-      nickname: "Ddfasfdsaaafsdafassaolly",
-      species: "Pinaafdsafsasfasahadggetgretnata",
-      h2ofrequency: "2x a jyjtyytt",
-    },
-    {
-      plant: "Dahlia fdsdsfstjtytyedafasfsafdsafsdafsaPinnata",
-      nickname: "Ddfasfdsaaehteyhresaolly",
-      species: "Pinaafdsafsateywqqdggetgretnata",
-      h2ofrequency: "2x a ewrqrqwerwqewqerwqweek",
-    },
-    {
-      plant: "Dahlia dfasfsarewqrwqqqdfasdfsdaffdsfsdafsaPinnata",
-      nickname: "Ddfytutrjmasfdsaasaolly",
-      species: "Pinaafdsafsafaaaadggetgretnata",
-      h2ofrequency: "2x aaafdasf ewrwqerwqweek",
-    },
-    {
-      plant: "Dahlia fdfdfafddsaj5jw5adsafsafdssfsdafsaPinnata",
-      nickname: "Ddfasfdsah56h5h5wasaolly",
-      species: "Pinaafdsatrewtegegfsadggetgretnata",
-      h2ofrequency: "2x a wtgerrtewwqewrwqerwqweek",
-    },
-    {
-      plant: "Dahlia fqrewqrq32dsfsadfasfsaaaafsdafsaPinnata",
-      nickname: "Ddfasftr34qt34yq34dsaasaolly",
-      species: "Pinaafdsafsady45uyqteteggetgretnata",
-      h2ofrequency: "2x a ewrtretewewqerwqweek",
-    },
-  ]);
+  return knex("plants").insert({
+    plant: "Dahlia Pinnata",
+    nickname: "Dolly",
+    species: "Pinnata",
+    h2ofrequency: "2x a week",
+  });
 };
